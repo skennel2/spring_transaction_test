a5055a2065f4f8c4a67a57a6b0c0d484e9099711
skennel2 <skennel2@gmail.com>
2018-11-08 00:34:24 +0900

    source cleanup

--- src/test/java/org/almansa/app/BookShopService.java
+++ src/test/java/org/almansa/app/BookShopService.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 @Component
 public class BookShopService {
@@ -15,5 +17,6 @@ public class BookShopService {
 	private PlatformTransactionManager transactionManager;
 		
 	public void addBooks() {
+		TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
 	}
 }
